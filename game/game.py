8dfe9a362359125ff591b84fba194e16b61731b6
Vaibhav Krishnakumar <vk714@ic.ac.uk>
2021-02-07 19:01:03 +0000

    Implement max tries for guesses

--- game/game.py
+++ game/game.py
@@ -1,5 +1,47 @@
 import sys,random
 
+def play(length):
+  WORDS = read_dictionary(length)
+  TOTAL_GUESSES = 5
+
+  playing = True
+
+  while playing:
+    secret = random.choice(WORDS).strip()
+    print("You have {} guesses; your {} letter Lingo starts with {}... ".format(TOTAL_GUESSES, length, secret[0]))
+    guessing = True
+    num_guesses = 0
+
+    while guessing and num_guesses < TOTAL_GUESSES:
+      guess = read_guess(length)
+      num_guesses = num_guesses + 1
+      print("Your guess was {}".format(guess))
+
+      if guess_is_correct(secret, guess):
+        print("You're right!")
+        guessing = False
+        num_guesses = 0
+        playing = play_again()
+      else:
+        greens = calculate_greens(secret, guess)
+        yellows = calculate_yellows(secret, guess)
+        if greens:
+          print("Incorrect, but you did get some letters right!\nThe word is of the form {}{}".format(secret[0], parse_greens(greens, guess, "_")))
+          if yellows:
+            yellows_str = ", ".join(yellows)
+            print("And the word contains these letters: {}".format(yellows_str))
+
+        elif yellows:
+          yellows_str = ", ".join(yellows)
+          print("Incorrect, but the word does contain these letters: {}".format(yellows_str))
+        else:
+          print("Incorrect, try again: {}{}".format(secret[0], parse_greens(set(), guess, "_")))
+    if num_guesses == TOTAL_GUESSES:
+      print("You're out of guesses! The word was {}".format(secret))
+      num_guesses = 0
+      guessing = False
+      playing = play_again()
+
 def read_dictionary(length):
   filename = "../dictionary/words_{}.txt".format(length)
   file = open(filename, "r")
@@ -7,45 +49,19 @@ def read_dictionary(length):
   file.close()
   return out
 
-def play(length):
-  words = read_dictionary(length)
-  playing = True
-  while playing:
-    secret = random.choice(words).strip()
-    print("Your {} letter Lingo starts with {}... ".format(length, secret[0]))
-    guessing = True
-    while guessing:
-      guess = read_guess(length)
-      print("Your guess was {}".format(guess))
-      if guess_is_correct(secret, guess):
-        play_again = raw_input("You're right!\nPlay again? Enter Y/N\n")
-        guessing = False
-        playing = "Y" == play_again.upper()
-      else:
-        greens = calculate_greens(secret, guess)
-        yellows = calculate_yellows(secret, guess)
-        if greens:
-          one, two, three = parse_greens(greens, guess, "_")
-          # TODO only works for 4 letter words, fix
-          print("Incorrect, but you did get some letters right!\nThe word is of the form {}{}{}{}".format(secret[0], one, two, three))
-          if yellows:
-            yellows_str = ", ".join(yellows)
-            print("And the word contains these letters: {}".format(yellows_str))
-        elif yellows:
-          yellows_str = ", ".join(yellows)
-          print("Incorrect, but the word does contain these letters: {}".format(yellows_str))
-        else:
-          # TODO only works for 4 letter words, fix
-          print("Incorrect, try again: {}___".format(secret[0]))
-
 def read_guess(length):
   guess = raw_input("Enter a guess\n").strip().lower()
+  # TODO Add check to verify word exists in dictionary
   while (len(guess) != length):
     guess = raw_input("{} isn't a valid guess, please enter a {} letter word!\n".format(guess, length)).strip().lower()
   return guess
 
 def guess_is_correct(secret, guess):
   return secret == guess
+
+def play_again():
+  play_again = raw_input("Play again? Enter Y/N\n")
+  return "Y" == play_again.upper()
 
 # Green refers to a correct letter in the correct index
 def calculate_greens(secret, guess):
@@ -63,14 +79,14 @@ def calculate_yellows(secret, guess):
       yellows.add(guess[i])
   return yellows
 
-# TODO only works for 4 letter words, fix
+# TODO Rename since not just used to parse greens
 def parse_greens(green, guess, default):
   one = guess[1] if 1 in green else default
   two = guess[2] if 2 in green else default
   three = guess[3] if 3 in green else default
-  return one, two, three
+  four = guess[4] if 4 in green else "" if len(guess) == 4 else default
+  return "".join((one, two, three, four))
 
 if __name__ == "__main__":
-  input = raw_input("Welcome to Lingo - choose 4 or 5 letter words to guess!:\n")
-  length = int(input)
-  play(length)
+  length = raw_input("Welcome to Lingo - choose 4 or 5 letter words to guess!:\n")
+  play(int(length))
